7f88e82447b339dd086f603dbbc2efa01f8a3a4a
@krikun1983 <akhmadeevst@gmail.com>
2021-08-04 00:43:34 +0400

    feat: add item add form

--- src/components/item-add-form/index.tsx
+++ src/components/item-add-form/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import { AddTodolistItemFuncProps } from '../../types/list-item';
 
 const ItemAddForm = ({ onAddTodolistItem }: AddTodolistItemFuncProps): JSX.Element => {
+  const [label, setText] = useState('');
+
+  const onLabelChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onAddTodolistItem(label);
+    setText('');
+  };
+
   return (
-    <div className="item-add-form">
-      <button className="item-add-form__btn" type="button" onClick={() => onAddTodolistItem('text')}>
+    <form className="item-add-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="item-add-form__control"
+        onChange={onLabelChange}
+        placeholder="what needs to be done"
+        value={label}
+      />
+      <button className="item-add-form__btn" type="submit">
         Add Item
       </button>
-    </div>
+    </form>
   );
 };
 

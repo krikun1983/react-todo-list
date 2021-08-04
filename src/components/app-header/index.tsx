5b4629d84865d218614a913d9b9b710d05a7c55e
@krikun1983 <akhmadeevst@gmail.com>
2021-08-04 16:01:08 +0400

    fix: style mobile

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -6,7 +6,7 @@ const AppHeader = ({ toDo, done }: AppHeaderToDone): JSX.Element => {
     <div className="app-header">
       <h1>My Todo List</h1>
       <h2>
-        {toDo} more to do, {done} done
+        {toDo} more to do, <span>{done} done</span>
       </h2>
     </div>
   );
